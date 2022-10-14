528ffdc9537835df9a0d1f6cb611036d2f2a82d5
Teminijesu <holarxmanuel@gmail.com>
2022-10-14 05:45:46 -0700

    Resolved annoying betty's remark

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_type));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
 
-	return 0;
+	return (0);
 }
