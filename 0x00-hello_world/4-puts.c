3b626c8cb2b1f04080d77b6826a08cf7cd0b71f7
Teminijesu <holarxmanuel@gmail.com>
2022-10-14 04:57:57 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
+	return (0);
+}
