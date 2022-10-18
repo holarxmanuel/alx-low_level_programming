9d9d7a312bb5b4314ada37d72f615a891ddd8f7d
Teminijesu <holarxmanuel@gmail.com>
2022-10-18 15:50:09 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * jack_bauer - printing every minute
+ * Return: 0
+ * void: arguement is void
+ *
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 50; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 53; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					if (i >= 50 && j >= 52)
+						break;
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
