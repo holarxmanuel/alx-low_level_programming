529699f36aa25896e93a29a500aed086370ff919
Teminijesu <holarxmanuel@gmail.com>
2022-10-18 14:15:31 -0700

    Diffrent method of writing the printing the last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,8 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	int last_digit_value = 48;
-	int i;
+	int last_digit_value = 48 + last_digit;
 
-	for (i = 0; i < last_digit; i++)
-	{
-		last_digit_value++;
-	}
 	_putchar(last_digit_value);
 	return (last_digit);
 }
