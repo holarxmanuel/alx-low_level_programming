71b16190d86878e34c479c0d81dd20177e705e27
Teminijesu <holarxmanuel@gmail.com>
2022-10-18 12:33:31 -0700

    Absloute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the arguement of the function
+ * Return: 0
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
