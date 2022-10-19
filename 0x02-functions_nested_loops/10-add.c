ac47f5415461319435081bca325d9d681088e373
Teminijesu <holarxmanuel@gmail.com>
2022-10-18 21:25:27 -0700

    ADD

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - this adds two integers
+ *
+ * Return: returns the result
+ * @a: is the first arguement
+ * @b: is the second arguement
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+	_putchar('\n');
+}
