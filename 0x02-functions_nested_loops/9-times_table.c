0046a6df4ecf724c8594884e107a54c0e3ccb882
Teminijesu <holarxmanuel@gmail.com>
2022-10-18 20:15:18 -0700

    Time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.c"
+
+/**
+ * times_table - this prints the 9 times table
+ *
+ *
+ * Reuturn: void
+ */
+void times_table(void)
+{
+	int number, multiply, product;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar('0');
+
+		for (multiply = 1; multiply <= 9; multiply++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+
+			_putchar((product / 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
