cc14ed917ca3a3e49adeba576b7e7766a5ad5fcc
Teminijesu <holarxmanuel@gmail.com>
2022-10-21 19:27:27 -0700

    Spacing the comments

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,13 +4,12 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
 
-
 	printf("Infinite loop incoming :(\n");
-
 
 	i = 0;
 	/**
@@ -20,7 +19,6 @@ int main(void)
 	* }
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 
 	return (0);
 }
