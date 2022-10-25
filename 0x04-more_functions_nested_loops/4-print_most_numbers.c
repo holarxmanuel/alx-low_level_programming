1d59932a78521fe72edf2832dae8ce6ebf84789a
Teminijesu <holarxmanuel@gmail.com>
2022-10-24 18:17:16 -0700

    Updated

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-	int lent = 0;
+	int i;
 
-	while (s[i] != '\0')
-		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
-	_putchar('\n');
+	for (i = '0'; i <= 10; i++)
+	{
+		if (!(i = 2 && i = 4))
+			_putchar(i);
+	}
 }
