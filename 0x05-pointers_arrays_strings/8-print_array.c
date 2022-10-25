db952e004d42c3f7afa4ee9a8bdd14aa4d601c16
Teminijesu <holarxmanuel@gmail.com>
2022-10-24 18:58:51 -0700

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,10 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n - 1)
-			printf(",");
+		if (i == n - 1)
+			continue;
+
+		printf(",");
 
 	}
 	printf("\n");
