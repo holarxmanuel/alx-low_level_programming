770303eb211964a9412e48ef0f38596c3bfc8ab0
Teminijesu <holarxmanuel@gmail.com>
2022-10-28 19:42:38 -0700

    upodate

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0, j = 0;
 
-	while (j == )
+	while (j == 0)
 	{
 		if  (*(s1 + i) == '\0') && (*(s2 + i) == '\0')
 			break;
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 int _putchar(char c);
+int _strcmp(char *s1, char *s2);
 #endif
