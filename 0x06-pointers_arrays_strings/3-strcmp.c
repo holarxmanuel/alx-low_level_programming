727a5909c5d2e854955e3d3dc9ef24d3c3981fcb
Teminijesu <holarxmanuel@gmail.com>
2022-10-28 19:23:10 -0700

    restart

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,13 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] !+ '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[j]);
+			return (s1[i] - s2[i]);
 		}
 	i++;
 	}
-	return (0);
+	return 0;
 }
