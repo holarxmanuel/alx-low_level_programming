1c7b3e0e16354911629291df78d50840568e51fb
Teminijesu <holarxmanuel@gmail.com>
2022-10-28 20:56:33 -0700

    git

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,34 +2,29 @@
 
 /**
  * cap_string - capitalizes
- * @strg: pointer
+ * @str: pointer
  * Return: pointer to strg
  */
-char *cap_string(char *strg)
+char *cap_string(char *str)
 {
-	int i = 0;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (strg[i])
+	if (*(str + count) >= 97 && *(str + count) <= 122)
+		*(str + count) = *(str + count) - 32;
+	count++;
+	while (*(str + count) != '\0')
 	{
-		while (!(strg[i] >= 'a' && strg[i] < = 'z'))
-			i++;
-		if (strg[i - 1] == ' ' ||
-		strg[i - 1] == '\t' ||
-		strg[i - 1] == '\n' ||
-		strg[i - 1] == ',' ||
-		strg[i - 1] == ';' ||
-		strg[i - 1] == '.' ||
-		strg[i - 1] == '!' ||
-		strg[i - 1] == '?' ||
-		strg[i - 1] == '"' ||
-		strg[i - 1] == '(' ||
-		strg[i - 1] == ')' ||
-		strg[i - 1] == '{' ||
-		strg[i - 1] == '}' ||
-		i == 0)
-			strg[i] -= 32;
-		i++;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(str + count) == sep_words[i])
+			{
+				if ((*(str + (count + 1)) >= 97) && (*(str + (count + 1)) <= 122))
+					*(str + (count + 1)) = *(str + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
-
-	return (strg);
+	return (str);
 }
