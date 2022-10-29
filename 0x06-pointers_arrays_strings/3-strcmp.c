40b5ea179f8728ccac6c41004db0584dca454185
Teminijesu <holarxmanuel@gmail.com>
2022-10-28 19:45:50 -0700

    allow git clear

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 
 	while (j == 0)
 	{
-		if  (*(s1 + i) == '\0') && (*(s2 + i) == '\0')
+		if  ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
 		j = *(s1 + i) - *(s2 + i);
 		i++;
