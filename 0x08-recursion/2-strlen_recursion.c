9b6d79365744389abc4adb4ee654506e76d5af5f
Teminijesu <holarxmanuel@gmail.com>
2022-11-02 19:22:18 -0700

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,8 @@
 int _strlen_recursion(char *s)
 {
 
-	if (*s != '\0')
-	{
-		int i;
-
-		i++;
-		_strlen_recursion(s + 1);
-	}
-	_putchar('\n');
-	return (i);
+	if (s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
