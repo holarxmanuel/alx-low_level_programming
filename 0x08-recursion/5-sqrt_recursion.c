82af6fd90801bd415d0928bdaff00b8859d77d8c
Teminijesu <holarxmanuel@gmail.com>
2022-11-02 20:14:03 -0700

    root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * power - this returns the square root of the number
+ * @n: input
+ * @c: iterator
+ * Return: square root of -1
+ */
+int power(int n, int c)
+{
+	if (c % (n / c) == n)
+	{
+		return (c);
+	else
+		return (-1);
+	}
+	return (0 + power(n, c + 1));
+}
+/**
+ * _sqrt_recursion - gitves the natural square root
+ * @n: number
+ * Return:  1 if it has a square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
