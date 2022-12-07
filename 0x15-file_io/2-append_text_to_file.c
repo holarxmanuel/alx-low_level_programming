1021f0f2103fe544eb2e3bf530fee28b926b55bb
Teminijesu <holarxmanuel@gmail.com>
2022-12-06 18:54:48 -0800

    Updated my buffer

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -37,12 +37,3 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	return (1);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Security
-    Status
-    Do
