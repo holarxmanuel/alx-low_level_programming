093001a0fd19694109ef208fe1a94f14b0df01b2
Olaleye Teminijesu <holarxmanuel@gmail.com>
2023-07-12 07:00:22 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int open, write, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+		       len++;
+	}
+	
+	open = open(filename, O_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	write = write(open, text_conent, len);
 
-	if (fd == -1)
-		return (-1);
+	if (open == -1 || write == -1)
+		return (1);
 
-	if (!text_content)
-		text_content = "";
-
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-	rwr = write(fd, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
+	close(open);
 
 	return (1);
 }
